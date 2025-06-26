692478998b5c19fba008df7541f0c0beaf75f9ed
madhusudhan123-star <dmadhusudhan98@gmail.com>
2025-06-26 16:36:29 +0530

    added

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors({
   origin: [
+    'https://dralcofree.com',
     'https://agent-sigma-livid.vercel.app',
     'https://sacredrelm.com',
     'https://myiandi.com',
